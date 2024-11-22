18100e494c5c6ff3ae5d9b9b3743c955457c840b
Yoo Seung-jun <funczun@gmail.com>
2024-11-22 16:42:19 +0900

    Add Problem7287.java

---
+++ src/algorithm/Problem7287.java
@@ -0,0 +1,10 @@
+// https://www.acmicpc.net/problem/7287
+
+package algorithm;
+
+public class Problem7287 {
+    public static void main(String[] args) {
+        System.out.println("86");
+        System.out.println("funczun");
+    }
+}
