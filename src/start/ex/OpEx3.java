bc91866f2cfca0da6bbd28f3a5b1da356a527ef0
Yoo Seung-jun <funczun@gmail.com>
2024-11-23 23:52:06 +0900

    Add Operator

---
+++ src/start/ex/OpEx3.java
@@ -0,0 +1,9 @@
+package start.ex;
+
+public class OpEx3 {
+    public static void main(String[] args) {
+        int score = 0;
+        boolean result = 80 <= score && score <= 100;
+        System.out.println(result);
+    }
+}
