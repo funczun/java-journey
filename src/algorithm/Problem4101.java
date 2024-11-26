33d5d760dae2ea946d3aa886edc3481181ce8906
Yoo Seung-jun <funczun@gmail.com>
2024-11-26 22:35:23 +0900

    Add Problem4101.java

---
+++ src/algorithm/Problem4101.java
@@ -0,0 +1,26 @@
+// https://www.acmicpc.net/problem/4101
+
+package algorithm;
+
+import java.util.Scanner;
+
+public class Problem4101 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+
+            if (n == 0 && m == 0) {
+                break;
+            }
+
+            if (n > m) {
+                System.out.println("Yes");
+            } else {
+                System.out.println("No");
+            }
+        }
+    }
+}
