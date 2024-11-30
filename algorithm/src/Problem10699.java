bf7e74878c9c4c1b2aa53256c0301c457419437a
Yoo Seung-jun <funczun@gmail.com>
2024-11-30 21:53:47 +0900

    Add Problem10699.java

---
+++ algorithm/src/Problem10699.java
@@ -0,0 +1,13 @@
+// https://www.acmicpc.net/problem/10699
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+public class Problem10699 {
+    public static void main(String[] args) {
+        ZonedDateTime seoulDateTime = ZonedDateTime.now(ZoneId.of("Asia/Seoul"));
+        LocalDate seoulDate = seoulDateTime.toLocalDate();
+        System.out.println(seoulDate);
+    }
+}
