2e13b9c59eb8d24f294593ee0a795be0a7fd659d
Yoo Seung-jun <funczun@gmail.com>
2024-12-06 20:19:22 +0900

    Add Problem24262.java

---
+++ algorithm/src/Problem24262.java
@@ -0,0 +1,8 @@
+// https://www.acmicpc.net/problem/24262
+
+public class Problem24262 {
+    public static void main(String[] args) {
+        System.out.println('1');
+        System.out.println('0');
+    }
+}
