1f04bf28c0de7f2448bee976e09281d82eec557e
Yoo Seung-jun <funczun@gmail.com>
2024-12-20 13:46:50 +0900

    Add Problem2587.java

---
+++ algorithm/src/Problem2587.java
@@ -0,0 +1,27 @@
+// https://www.acmicpc.net/problem/2587
+
+import java.io.*;
+
+public class Problem2587 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] nums = new int[5];
+        int sum = 0;
+
+        for (int i = 0; i < 5; i++) {
+            nums[i] = Integer.parseInt(br.readLine());
+            sum += nums[i];
+        }
+        for (int i = 0; i < 5; i++) {
+            for (int j = i + 1; j < 5; j++) {
+                if (nums[i] > nums[j]) {
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+        System.out.println(sum / 5);
+        System.out.println(nums[2]);
+    }
+}
