346e0e903756d283f0288ac52f2ae450ad39df61
Yoo Seung-jun <funczun@gmail.com>
2024-12-22 20:37:28 +0900

    Add Problem2751.java

---
+++ algorithm/src/Problem2751.java
@@ -0,0 +1,22 @@
+// https://www.acmicpc.net/problem/2751
+
+import java.io.*;
+import java.util.Arrays;
+
+public class Problem2751 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] nums = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(nums);
+
+        for (int i = 0; i < N; i++) {
+            System.out.println(nums[i]);
+        }
+        br.close();
+    }
+}
