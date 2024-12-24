1e2bb136bd466fdc53eb9094f266afe0fa1bcb29
Yoo Seung-jun <funczun@gmail.com>
2024-12-25 00:44:28 +0900

    Add pack

---
+++ basic/src/pack/a/User.java
@@ -0,0 +1,7 @@
+package pack.a;
+
+public class User {
+    public User() {
+        System.out.println("package pack.a 유저 생성");
+    }
+}
---
+++ basic/src/pack/b/User.java
@@ -0,0 +1,7 @@
+package pack.b;
+
+public class User {
+    public User() {
+        System.out.println("package pack.b 유저 생성");
+    }
+}
