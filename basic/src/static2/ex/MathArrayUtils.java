03eb007d2d97983f777bff670125ee055473c56f
Yoo Seung-jun <funczun@gmail.com>
2025-01-03 23:54:09 +0900

    Add static2

---
+++ basic/src/static2/ex/MathArrayUtils.java
@@ -0,0 +1,39 @@
+package static2.ex;
+
+public class MathArrayUtils {
+    // 인스턴스 생성 금지
+    private MathArrayUtils() {
+    }
+
+    public static int sum(int[] array) {
+        int total = 0;
+        for (int i = 0; i < array.length; i++) {
+            total += array[i];
+        }
+        return total;
+    }
+
+    public static double average(int[] array) {
+        return (double) sum(array) / array.length;
+    }
+
+    public static int min(int[] array) {
+        int min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (min > array[i]) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+
+    public static int max(int[] array) {
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (max < array[i]) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+}
