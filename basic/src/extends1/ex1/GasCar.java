f45d779fd09af52fb7a7b9e501748b6c70b0397f
Yoo Seung-jun <funczun@gmail.com>
2025-01-05 22:34:47 +0900

    Add extends1

---
+++ basic/src/extends1/ex1/GasCar.java
@@ -0,0 +1,12 @@
+package extends1.ex1;
+
+public class GasCar {
+
+    public void move() {
+        System.out.println("Gas-Car moving");
+    }
+
+    public void fillUp() {
+        System.out.println("Gas-Car filling up");
+    }
+}
---
+++ basic/src/extends1/ex3/Car.java
@@ -0,0 +1,13 @@
+package extends1.ex3;
+
+public class Car {
+
+    public void move() {
+        System.out.println("Car move");
+    }
+
+    // 추가
+    public void openDoor() {
+        System.out.println("Open door");
+    }
+}
