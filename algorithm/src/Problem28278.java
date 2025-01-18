d4d18bae6cb646169b00e18223a1a4debdb1a33b
Yoo Seung-jun <funczun@gmail.com>
2025-01-19 06:39:17 +0900

    Add Problem28278.java

---
+++ algorithm/src/Problem28278.java
@@ -0,0 +1,55 @@
+// https://www.acmicpc.net/problem/28278
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Problem28278 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        Stack<Integer> stack = new Stack<>();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int option = Integer.parseInt(st.nextToken());
+
+            switch (option) {
+                case 1:
+                    stack.push(Integer.parseInt(st.nextToken()));
+                    break;
+                case 2:
+                    if (stack.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(stack.pop()).append("\n");
+                    }
+                    break;
+                case 3:
+                    sb.append(stack.size()).append("\n");
+                    break;
+                case 4:
+                    if (stack.isEmpty()) {
+                        sb.append(1).append("\n");
+                    } else {
+                        sb.append(0).append("\n");
+                    }
+                    break;
+                case 5:
+                    if (stack.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(stack.peek()).append("\n");
+                    }
+                    break;
+            }
+        }
+
+        System.out.print(sb);
+    }
+}
