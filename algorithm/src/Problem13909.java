07c6c1833e9963602596facc66b707700525336d
Yoo Seung-jun <funczun@gmail.com>
2025-02-09 22:25:39 +0900

    Add Problem13909.java

---
+++ algorithm/src/Problem13909.java
@@ -0,0 +1,15 @@
+// https://www.acmicpc.net/problem/13909
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem13909 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        System.out.println((int) Math.sqrt(N));
+    }
+}
