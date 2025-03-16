b7aefc31bba89a7f10a1d7931fb3df612ff02dc1
Yoo Seung-jun <funczun@gmail.com>
2025-03-16 15:21:52 +0900

    Refactor: BOJ 9461

--- algorithm/src/Main.java
+++ algorithm/src/Main.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    static int[] dp;
+    static long[] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,9 +18,12 @@ public class Main {
     }
 
     private static void getP(int n) {
-        dp = new int[n + 1];
+        dp = new long[n + 1];
         dp[1] = 1;
-        dp[2] = 1;
+
+        if (n > 1) {
+            dp[2] = 1;
+        }
 
         for (int i = 3; i <= n; i++) {
             dp[i] = dp[i - 3] + dp[i - 2];
