ba41f2a55a0fc7ad66591b3a8e06720e7bd85b5a
Yoo Seung-jun <funczun@gmail.com>
2025-03-19 10:42:13 +0900

    Add: BOJ 11726

--- algorithm/src/Main.java
+++ algorithm/src/Main.java
@@ -4,50 +4,23 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int start;
-    static int end;
+    static int[] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
 
-        int[] trees = new int[N];
-
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            trees[i] = Integer.parseInt(st.nextToken());
-            if (trees[i] > end) {
-                end = trees[i];
-            }
+        dp = new int[n + 1];
+        dp[1] = 1;
+        if (n > 1) {
+            dp[2] = 2;
         }
 
-        binary(trees, M);
-    }
-
-    static void binary(int[] trees, int M) {
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            long sum = 0;
-
-            for (int tree : trees) {
-                int afterCut = tree - mid;
-                if (afterCut > 0) {
-                    sum += afterCut;
-                }
-            }
-
-            if (sum > M) {
-                start = mid + 1;
-            } else if (sum < M) {
-                end = mid - 1;
-            } else {
-                System.out.print(mid);
-                return;
-            }
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 2] + dp[i - 1]) % 10007;
         }
 
-        System.out.println(end);
+        System.out.print(dp[n]);
     }
 }
