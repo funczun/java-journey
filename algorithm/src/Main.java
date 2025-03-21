accb0d03f726da7dade5217657b18881f6adb3d4
Yoo Seung-jun <funczun@gmail.com>
2025-03-21 20:33:45 +0900

    Add: BOJ 11727

--- algorithm/src/Main.java
+++ algorithm/src/Main.java
@@ -1,24 +1,17 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
 
 public class Main {
     static int[] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-
+        int n = Integer.parseInt(br.readLine());
         dp = new int[n + 1];
-        dp[1] = 1;
-        if (n > 1) {
-            dp[2] = 2;
-        }
 
-        for (int i = 3; i <= n; i++) {
-            dp[i] = (dp[i - 2] + dp[i - 1]) % 10007;
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = (dp[i - 1] + (dp[i - 2] * 2)) % 10007;
         }
 
         System.out.print(dp[n]);
