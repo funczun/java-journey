f126901103e25a600ee4eab487dcb772e0dacec0
Yoo Seung-jun <funczun@gmail.com>
2025-03-22 17:02:07 +0900

    Add: BOJ 2178

--- algorithm/src/Main.java
+++ algorithm/src/Main.java
@@ -1,19 +1,58 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int[] dp;
+    static int[][] matrix;
+    static int[][] visited;
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {1, -1, 0, 0};
+    static int N, M;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        dp = new int[n + 1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        dp[0] = 1;
-        dp[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            dp[i] = (dp[i - 1] + (dp[i - 2] * 2)) % 10007;
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        matrix = new int[N][M];
+        visited = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < M; j++) {
+                matrix[i][j] = line.charAt(j) - '0';
+            }
         }
 
-        System.out.print(dp[n]);
+        bfs(0, 0);
+    }
+
+    static void bfs(int startX, int startY) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{startX, startY});
+        visited[startX][startY] = 1;
+
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            int x = cur[0];
+            int y = cur[1];
+
+            if (x == N - 1 && y == M - 1) {
+                System.out.println(visited[x][y]);
+                return;
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int newX = x + dx[i];
+                int newY = y + dy[i];
+
+                if (newX >= 0 && newX < N && newY >= 0 && newY < M
+                        && visited[newX][newY] == 0 && matrix[newX][newY] == 1) {
+                    queue.offer(new int[]{newX, newY});
+                    visited[newX][newY] = visited[x][y] + 1;
+                }
+            }
+        }
     }
 }
