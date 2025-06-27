e79cce6343b6a162919be65e80df7969da42d1d8
Yoo Seung-jun <funczun@gmail.com>
2025-06-27 23:46:26 +0900

    chore: solve daily algorithm

--- algorithm/src/Main.java
+++ algorithm/src/Main.java
@@ -2,58 +2,35 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[][] graph;
-    static int[] visited;
-    static int result;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(br.readLine());
 
-        graph = new int[N + 1][N + 1];
-        visited = new int[N + 1];
-
-        for (int i = 1; i < M + 1; i++) {
-            st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-
-            graph[start][end] = 1;
-            graph[end][start] = 1;
-        }
-
-        count(graph, 1);
-        System.out.print(result);
-    }
-
-    static void count(int[][] graph, int start) {
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(start);
-
-        while (!q.isEmpty()) {
-            int cur = q.poll();
-
-            if (visited[cur] == 1) {
-                continue;
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>((a, b) -> {
+            if (Math.abs(a) == Math.abs(b)) {
+                return a - b;
             }
+            return Math.abs(a) - Math.abs(b);
+        });
 
-            visited[cur] = 1;
+        StringBuilder sb = new StringBuilder();
 
-            for (int i = 1; i < graph[cur].length; i++) {
-                if (graph[cur][i] == 1) {
-                    q.offer(i);
+        for (int i = 0; i < N; i++) {
+            int value = Integer.parseInt(br.readLine());
+
+            if (value != 0) {
+                minHeap.add(value);
+            }
+            else {
+                if (minHeap.isEmpty()) {
+                    sb.append(0).append("\n");
+                }
+                else {
+                    sb.append(minHeap.remove()).append("\n");
                 }
             }
         }
 
-        result++;
-
-        for (int i = 1; i < visited.length; i++) {
-            if (visited[i] == 0) {
-                count(graph, i);
-            }
-        }
+        System.out.print(sb);
     }
 }
