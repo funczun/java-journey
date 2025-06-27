560cdc6a0ecc991e8049ab1e8f1a569ebe5f31a2
Yoo Seung-jun <funczun@gmail.com>
2025-06-27 22:42:40 +0900

    chore: solve daily algorithm

--- algorithm/src/Main.java
+++ algorithm/src/Main.java
@@ -2,27 +2,58 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int[][] graph;
+    static int[] visited;
+    static int result;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>(); // 기본형: minHeap
-        StringBuilder sb = new StringBuilder();
+        graph = new int[N + 1][N + 1];
+        visited = new int[N + 1];
 
-        for (int i = 0; i < N; i++) {
-            int value = Integer.parseInt(br.readLine());
-            if (value > 0) {
-                pq.add(value);
+        for (int i = 1; i < M + 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            graph[start][end] = 1;
+            graph[end][start] = 1;
+        }
+
+        count(graph, 1);
+        System.out.print(result);
+    }
+
+    static void count(int[][] graph, int start) {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(start);
+
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+
+            if (visited[cur] == 1) {
+                continue;
             }
-            else if (value == 0) {
-                try {
-                    sb.append(pq.remove()).append("\n");
-                }
-                catch (Exception e) {
-                    sb.append(0).append("\n");
+
+            visited[cur] = 1;
+
+            for (int i = 1; i < graph[cur].length; i++) {
+                if (graph[cur][i] == 1) {
+                    q.offer(i);
                 }
             }
         }
-        System.out.print(sb);
+
+        result++;
+
+        for (int i = 1; i < visited.length; i++) {
+            if (visited[i] == 0) {
+                count(graph, i);
+            }
+        }
     }
 }
